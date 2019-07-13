3b692c93d2d17905a8a13261c671992d3fb0fd26
Aiden <asdf62210@gmail.com>
2019-07-13 13:05:47 +0800

    顯示文字

---
+++ app/src/main/java/com/example/ch02_001/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.ch02_001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "按鍵點擊", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
